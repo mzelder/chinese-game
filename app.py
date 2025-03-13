f582d4855c9b5a961cab2b448731a6497343e035
oheyek <oheyek@proton.me>
2025-03-13 14:27:47 +0100

    Podstawowy routing pod setup serwera

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/create_lobby', methods=['POST'])
+def create_lobby():
+    return render_template('lobby.html')
+
+
+@app.route('/join_lobby', methods=['POST'])
+def join_lobby():
+    return render_template('lobby.html')
+
+
+@app.route('/join_lobby_option', methods=['POST'])
+def join_lobby_option():
+    return render_template('joininglobby.html')
+
+
+@app.route('/start_game', methods=['POST'])
+def start_game():
+    return render_template('game.html')

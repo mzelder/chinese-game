01e0b3a9f04d5842f406a8a4db64cbe980a02a3c
KacperNowo <k.nowosinski@gsuite.us.edu.pl>
2025-03-18 06:45:03 +0100

    removed id generation client side

--- client/static/script/script.js
+++ client/static/script/script.js
@@ -1,13 +1,3 @@
-function idGeneration(){
-    let id = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const length = 10;
-    for (let i=0; i<length; i++) {
-        id += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    document.getElementById('uniqueId').textContent = "Your lobby's ID:" + id;
-};
-
 const loadGradientScript = () => {
     let script = document.createElement("script");
     script.src = "https://cdn.jsdelivr.net/gh/greentfrapp/pocoloco@minigl/minigl.js";
